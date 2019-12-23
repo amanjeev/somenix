1cfd9bf2c2bde6c77c3cbbe568538986ecbca1db
Amanjeev Sethi <aj@amanjeev.com>
2019-12-22 23:10:34 -0500

    adds torontula x1c

---
+++ nixos/roots/torontula/configuration.nix
@@ -0,0 +1,93 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ lib, config, pkgs, callPackage, ... }:
+
+
+let
+  home-manager = builtins.fetchGit {
+    url = "https://github.com/rycee/home-manager.git";
+    rev = "dff5f07952e61da708dc8b348ea677414e992215"; 
+    ref = "release-19.09";
+  };
+
+  nixos-hardware =
+    fetchTarball https://github.com/NixOS/nixos-hardware/archive/master.tar.gz;
+in
+{
+
+  imports = [ 
+    "${home-manager}/nixos"
+    # TODO: change this for other machines
+    #"${nixos-hardware}/lenovo/thinkpad/x1"
+    ./hardware-configuration.nix
+    ../../home.nix
+    ../../common.nix
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.luks.devices = [
+  {
+    name = "root";
+    device = "/dev/nvme0n1p2";
+    preLVM = true;
+  }
+  ];
+
+  networking = {
+    hostName = "torontula";
+    useDHCP = false;
+    wireless = {
+      enable = false;  # Enables wireless support via wpa_supplicant.
+    };
+    interfaces = {
+      enp0s20f0u1u2u1 = {
+        useDHCP = true;
+      };
+      wlp2s0 = {
+        useDHCP = true;
+      };
+      enp0s20f0u2 = { useDHCP = true; };
+      enp0s31f6 = { useDHCP = true; };
+      enp59s0u1u1 = { useDHCP = true; };
+
+    };
+    networkmanager = {
+      enable = true;
+      # Plausible MAC randomization
+      ethernet.macAddress = "random";
+      wifi.macAddress = "random";
+      extraConfig = ''
+        [connection-extra]
+        ethernet.generate-mac-address-mask=FE:FF:FF:00:00:00
+        wifi.generate-mac-address-mask=FE:FF:FF:00:00:00
+      '';
+    };
+  };
+
+  services = {
+    xserver = {
+      enable = true;
+      layout = "us";
+      libinput = {
+        enable = true;
+        naturalScrolling = true;
+        middleEmulation = true;
+        tapping = true;
+      };
+    };
+  };
+
+  # This value determines the NixOS release with which your system is to be
+  # compatible, in order to avoid breaking some software such as database
+  # servers. You should change this only after NixOS release notes say you
+  # should.
+  system.stateVersion = "19.09"; # Did you read the comment?
+
+}
+
