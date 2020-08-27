f1c937d3a3e6173bcb5d70f1acc0341a4bc297b6
Amanjeev Sethi <aj@amanjeev.com>
2020-08-27 13:00:58 -0400

    adds: microsoft teams

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -90,6 +90,7 @@
     syncthing
     tdesktop
     tealdeer  # is a very fast implementation of tldr, a command-line program for displaying simplified, example based and community-driven man pages
+    teams
     thefuck
     tmux
     tmuxPlugins.continuum
