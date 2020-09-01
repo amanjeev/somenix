a4d93f92399de6208b78c57bb0909c4dbdb6b1d7
Amanjeev Sethi <aj@amanjeev.com>
2020-09-01 18:14:04 -0400

    adds: fish bass plugin

--- nixos/pkgs/default.nix
+++ nixos/pkgs/default.nix
@@ -9,6 +9,8 @@
   xdg.configFile."kitty/kitty.conf".source = ./confs/kitty.conf;
   xdg.configFile."alacritty/alacritty.yml".source = ./confs/alacritty.yml;
   xdg.configFile."starship.toml".source = ./confs/starship.toml;
+  xdg.configFile."fish/functions/__bass.py".source = ./confs/fish/functions/__bass.py;
+  xdg.configFile."fish/functions/bass.fish".source = ./confs/fish/functions/bass.fish;
 
   home.packages = with pkgs; [
     alacritty
