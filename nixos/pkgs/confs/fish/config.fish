acc6e644727b2d893593bf0804f827cd521fa33c
Amanjeev Sethi <aj@amanjeev.com>
2021-07-17 18:58:30 -0400

    fish: updates config

--- nixos/pkgs/confs/fish/config.fish
+++ nixos/pkgs/confs/fish/config.fish
@@ -1,20 +1,10 @@
 # Fix some utf-8 errors
 set -gx LC_ALL en_CA.UTF-8
 
-# Make git use emacs
-set -x EDITOR kak
-
 # gnome-keyring insists on setting this to itself, even if ssh support is disabled
 set -x SSH_AUTH_SOCK "/run/user/1000/gnupg/S.gpg-agent.ssh"
 
-# Better nix-shell support!
-any-nix-shell fish --info-right | source
- 
 # tuuuuuuuurbofish!
 set fish_greeting 'Welcome to the '(set_color FA1100)'::<>' (set_color normal)'...'
 
-# python virtualenvwrapper
-set -g -x WORKON_HOME ~/virtualenvs
-
 eval (direnv hook fish)
-starship init fish | source
