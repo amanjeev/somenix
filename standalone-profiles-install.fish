c8665c4ba3703a7f1b9948e81bb3af81b1ae8be0
Amanjeev Sethi <aj@amanjeev.com>
2023-03-05 17:22:05 -0500

    standalone profile install packages added from cargo

--- standalone-profiles-install.fish
+++ standalone-profiles-install.fish
@@ -1,6 +1,24 @@
 #!/usr/bin/env fish
 
-set pkgs "neovim" "cowsay"
+set pkgs "topgrade" \
+    "alacritty" \
+    "bat" \
+    "cargo-flamegraph" \
+    "cargo-temp" \
+    "du-dust" \
+    "exa" \
+    "fd" \
+    "flamegraph" \
+    "fx" \
+    "git-extras" \
+    "hyperfine" \
+    "mdbook" \
+    "procs" \
+    "ripgrep" \
+    "ripgrep-all" \
+    "sd" \
+    "starship"
+
 
 for pkg in $pkgs
     nix profile install "nixpkgs#$pkg"
