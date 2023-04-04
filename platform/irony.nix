6fc40b5bc09766e0f83ce09c650de11b56d6692c
Amanjeev Sethi <aj@amanjeev.com>
2023-04-03 20:30:02 -0400

    irony: bluetooth

--- platform/irony.nix
+++ platform/irony.nix
@@ -47,6 +47,7 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
   # hardware.video.hidpi.enable = lib.mkDefault true;
+  hardware.bluetooth.enable = true;
 
   networking.hostName = "irony"; # Define your hostname.
   networking.networkmanager.enable = true;
