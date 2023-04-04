c77c3c38819b5c0cbcf2c8988cfe257c67fc910b
Amanjeev Sethi <aj@amanjeev.com>
2023-04-03 22:41:12 -0400

    irony: fish config

--- packages/fish/config.fish
+++ packages/fish/config.fish
@@ -3,7 +3,44 @@ set -gx LC_ALL en_CA.UTF-8
 
 fish_add_path -g ~/.cargo/bin
 fish_add_path -g ~/bin
-   
+
+setenv SSH_ENV $HOME/.ssh/environment
+
+function start_agent                                                                                                                                                                    
+    echo "Initializing new SSH agent ..."
+    ssh-agent -c | sed 's/^echo/#echo/' > $SSH_ENV
+    echo "succeeded"
+    chmod 600 $SSH_ENV 
+    . $SSH_ENV > /dev/null
+    ssh-add
+end
+
+function test_identities                                                                                                                                                                
+    ssh-add -l | grep "The agent has no identities" > /dev/null
+    if [ $status -eq 0 ]
+        ssh-add
+        if [ $status -eq 2 ]
+            start_agent
+        end
+    end
+end
+
+if [ -n "$SSH_AGENT_PID" ] 
+    ps -ef | grep $SSH_AGENT_PID | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    end  
+else
+    if [ -f $SSH_ENV ]
+        . $SSH_ENV > /dev/null
+    end  
+    ps -ef | grep $SSH_AGENT_PID | grep -v grep | grep ssh-agent > /dev/null
+    if [ $status -eq 0 ]
+        test_identities
+    else 
+        start_agent
+    end  
+end
 
 # tuuuuuuuurbofish!
 set fish_greeting 'Welcome to the '(set_color FA1100)'::<>' (set_color normal)'...'
