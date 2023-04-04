987245e6a391dd6f8441a7b13168144bb8ab9ae1
Amanjeev Sethi <aj@amanjeev.com>
2023-04-03 21:11:33 -0400

    irony: conf fixes

--- packages/fish/config.fish
+++ packages/fish/config.fish
@@ -3,15 +3,9 @@ set -gx LC_ALL en_CA.UTF-8
 
 fish_add_path -g ~/.cargo/bin
 fish_add_path -g ~/bin
+   
 
 # tuuuuuuuurbofish!
 set fish_greeting 'Welcome to the '(set_color FA1100)'::<>' (set_color normal)'...'
-
-# Ruby rbenv
-set -x PATH $HOME/.rbenv/bin $PATH
-rbenv init - | source
-status --is-interactive; and source (rbenv init -|psub)
-
-direnv hook fish | source
 
 starship init fish | source
