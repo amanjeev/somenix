48236b3d621983c620c2dbdb00cf115a0f68cfed
Amanjeev Sethi <aj@amanjeev.com>
2023-06-03 11:04:25 -0400

    move to 23.05. updates

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
     description = "AJ's Flake";
 
     inputs = {
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+        nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
     };
 
